6fc2d810dbb00952fafb47761451d3978d3276df
Urja Tendolkar <tendolkarurja@gmail.com>
2025-04-27 23:57:26 +0530

    Added the recommendation webpage successfully, along with other changes

--- recommender/views.py
+++ recommender/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_recommendations(request):
-    papers = [
-        {'title': 'AI in Healthcare', 'authors': 'John Doe, Jane Smith', 'link': 'https://example.com/paper1'},
-        {'title': 'Machine Learning for Climate Change', 'authors': 'Alice Johnson', 'link': 'https://example.com/paper2'},
-        {'title': 'Quantum Computing and AI', 'authors': 'Bob Lee', 'link': 'https://example.com/paper3'}
-    ]
-    return render(request, 'recommender/recommend.html', {'papers':papers})
+    if request.method == "POST":
+        choice = request.POST.get('ch')
+        return render(request, 'recommender/recommend.html', {'selected_choice' : choice})
+    
