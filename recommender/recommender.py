3934a510a55c57199713b546c49cba24fd4e0193
Urja Tendolkar <tendolkarurja@gmail.com>
2025-05-09 12:42:45 +0530

    Topic suggestions page functionality enhanced

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -1,9 +1,1 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def recommend(user_interests, paper_abstracts):
-    embeddings1 = model.encode(user_interests, convert_to_tensor=True)
-    embeddings2 = model.encode(paper_abstracts, convert_to_tensor=True)
-    scores = util.cos_sim(embeddings1, embeddings2)
-    return scores
+from scraper.models import ArxivPaper
